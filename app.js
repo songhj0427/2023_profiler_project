516714624e9a97449dd4448392a93024e387cec2
songhj <427shg@naver.com>
2023-03-27 19:57:32 +0900

    feat: get요청이 들어올 때 index화면을 볼 수 있게 기능 추가

--- app.js
+++ app.js
@@ -4,27 +4,31 @@ var multer = require("multer");
 var fs = require("fs");
 
 app = express();
-const PORT = process.env.PORT || 3000;
+app.set("port", process.env.PORT || 3000);
 
 var upload = multer({
   storage: multer.diskStorage({
-    destination: (req, file, callback) => {
-      callback(null, "uploads/");
+    destination: (req, file, done) => {
+      done(null, "data/");
     },
-    filename: (req, file, callback) => {
-      callback(
-        null,
-        `${path.basename(file.originalname) + path.extname(file.originalname)}`
-      );
+    filename: (req, file, done) => {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + ext);
     },
   }),
 });
 
+//get요청이 들어오면 시작화면을 보여줌
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+//받은 파일을 multer에 지정된 디렉터리위치에 filename으로 저장함.
 app.post("/uploadFile", upload.single("dataFile"), (req, res) => {
   const file = req.file;
   res.send("파일 받음");
 });
 
-app.listen(PORT, () => {
+app.listen(app.get("port"), () => {
   console.log("3000포트에서 대기 중");
 });
