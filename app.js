dcdf49729420e9e56641a6bd021fd5447768d0ec
songhj <427shg@naver.com>
2023-03-29 19:38:30 +0900

    test

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require("express");
 var path = require("path");
 var multer = require("multer");
+var mysql = require("mysql");
 var fs = require("fs");
+var readline = require("readline");
 
 app = express();
 app.set("port", process.env.PORT || 3000);
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "fjdkslvn107!",
+  database: "nodejs",
+});
+connection.connect();
 
 var upload = multer({
   storage: multer.diskStorage({
@@ -12,7 +22,7 @@ var upload = multer({
       done(null, "data/");
     },
     filename: (req, file, done) => {
-      const ext = path.extname(file.originalname);
+      var ext = path.extname(file.originalname);
       done(null, path.basename(file.originalname, ext) + ext);
     },
   }),
@@ -25,10 +35,13 @@ app.get("/", (req, res) => {
 
 //받은 파일을 multer에 지정된 디렉터리위치에 filename으로 저장함.
 app.post("/uploadFile", upload.single("dataFile"), (req, res) => {
-  const file = req.file;
+  console.log(res.file.originalname);
+  var filename = "./data/index.html";
+  var instream = fs.createReadStream(filename);
+  var reader = readline.createInterface(instream, process.stdout);
   res.send("파일 받음");
 });
 
 app.listen(app.get("port"), () => {
-  console.log("3000포트에서 대기 중");
+  console.log(`${app.get("port")}번 포트에서 대기 중...`);
 });
